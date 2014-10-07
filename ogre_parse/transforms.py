30550a16b574c75f0fa59b7df6246bf2eb4abb22
John K. Grant <cyrfer@gmail.com>
2014-10-07 14:42:09 -0700

    more customized Split transform.

--- ogre_parse/transforms.py
+++ ogre_parse/transforms.py
@@ -13,26 +13,34 @@ class SplitPass(object):
         # can configure these
         self.split_iteration = 'once_per_light'
 
+        self.shader_per_texture_vert_ambient = {0: 'phong',
+                                                1: 'phong_1UV_fog',
+                                                2: 'phong_1UV_fog'}
+
+        self.shader_per_texture_frag_ambient = {0: 'BP_fog',
+                                                1: 'BP_fog_decal',
+                                                2: 'BP_fog_decal_emissive'}
+
         # the default shaders if the pass does not use any
         self.shader_per_texture_vert = {0: 'phong',
-                                        1: 'phong_1UV_fog_tangent',
+                                        1: 'phong_1UV_fog',
                                         2: 'phong_1UV_fog_tangent',
-                                        3: 'phong_1UV_fog_tangent',
-                                        4: 'phong_1UV_fog_tangent'}
+                                        3: 'phong_1UV_fog_tangent'}
+                                        # 4: 'phong_1UV_fog_tangent'} # should not need 4
 
         self.shader_per_texture_frag = {0: 'BP_fog',
                                         1: 'BP_fog_decal',
                                         2: 'BP_fog_decal_normal',
-                                        3: 'BP_fog_decal_normal_specular',
-                                        4: 'BP_fog_decal_emissive_normal_specular'}
+                                        3: 'BP_fog_decal_normal_specular'}
+                                        # 4: 'BP_fog_decal_emissive_normal_specular'} # should not need 4
 
     def log(self, msg, mname):
-        logtext = 'Material [%s] had an issue: %s' % (mname, msg)
+        logtext = 40*'*' + '\nMaterial [%s] had an issue: %s' % (mname, msg) + '\n' + 40*'*'
         logging.info(logtext)
         print(logtext)
 
-    def add_shaders(self, p, mname):
-        self.log(10*'-' + 'adding shaders' + 10*'-', mname)
+    def add_shaders_direct(self, p, mname):
+        # self.log(10*'-' + 'adding shaders' + 10*'-', mname)
 
         sh_vert = ogre_parse.submodel.MShaderRef()
         sh_vert.stage = 'vertex_program_ref'
@@ -45,6 +53,20 @@ class SplitPass(object):
         p.shaders.append( sh_vert )
         p.shaders.append( sh_frag )
 
+    def add_shaders_ambient(self, p, mname):
+        # self.log(10*'-' + 'adding shaders' + 10*'-', mname)
+
+        sh_vert = ogre_parse.submodel.MShaderRef()
+        sh_vert.stage = 'vertex_program_ref'
+        sh_vert.resource_name = self.shader_per_texture_vert_ambient[len(p.texture_units)]
+
+        sh_frag = ogre_parse.submodel.MShaderRef()
+        sh_frag.stage = 'fragment_program_ref'
+        sh_frag.resource_name = self.shader_per_texture_frag_ambient[len(p.texture_units)]
+
+        p.shaders.append( sh_vert )
+        p.shaders.append( sh_frag )
+
     def split_pass(self, tech, mname):
         # assumes just a single pass exists
         if len(tech.passes) > 1:
@@ -52,9 +74,10 @@ class SplitPass(object):
 
         pass0 = tech.passes[0] # the ambient pass
 
-        if len(pass0.shaders) < 2:
-            pass0.shaders.clear() # sometimes a pass has just a frag shader
-            self.add_shaders(pass0, mname)
+        # we need to clear the shaders later, so no need for this anymore.
+        # if len(pass0.shaders) < 2:
+        #     pass0.shaders.clear() # sometimes a pass has just a frag shader
+        #     self.add_shaders(pass0, mname)
 
         pass1 = copy.deepcopy(pass0) # the per-light pass
 
@@ -62,21 +85,52 @@ class SplitPass(object):
         pass1.name = 'per_light'
         pass1.iteration = self.split_iteration
         pass1.illumination_stage = 'per_light'
-        pass1.scene_blend = 'add'
+        if pass1.scene_blend == 'one zero':
+            pass1.scene_blend = 'add'
+        # don't log these because they are known and common
+        # elif (pass1.scene_blend != 'src_alpha one_minus_src_alpha') and (pass1.scene_blend != 'alpha_blend'):
+        else:
+            logtext = '''
+----------------------------------------
+  material [%s] is using scene_blend = [%s].
+----------------------------------------''' % (mname, pass1.scene_blend)
+            logging.info(logtext)
+            print(logtext)
+
+        pass1.depth_write = 'off'    # no need to overwrite
+        pass1.depth_func = 'equal'   # better be the same, might help with flickering
+        pass1.depth_check = 'on'     # on is default, but making sure here.
+
+        # fix the shaders
+        pass1.shaders.clear()
+        if len(pass1.texture_units) == 4:  # sometimes the body has 4 textures by mistake (D,E,N,S)
+            del pass1.texture_units[1]  # remove E, we only require D,N,S for this per-light pass
+        if len(pass1.texture_units) == 3:  # the DNS (body) material is messing up with 3 textures
+            del pass1.texture_units[2:]  # only use 2 textures (D,N) instead of 3 (D,N,S)
+        self.add_shaders_direct(pass1, mname)
 
         # turn OFF ambient
         pass1.ambient = ogre_parse.basemodel.Color(vals=[0, 0, 0, 1])
         pass1.emissive = ogre_parse.basemodel.Color(vals=[0, 0, 0, 1])
 
-        # # turn ON other colors
-        # pass1.diffuse = pass0.diffuse
-        # pass1.specular = pass0.specular
+        # turn ON other colors
+        pass1.diffuse = ogre_parse.basemodel.Color(vals=[1, 1, 1, 1])
+        pass1.specular = ogre_parse.basemodel.Color(vals=[1, 1, 1, 1])
 
         # adjust properties on pass0
         pass0.name = 'ambient'
         pass0.iteration = 'once'
         pass0.illumination_stage = 'ambient'
-        pass0.scene_blend = 'one zero'
+        if pass0.scene_blend == 'one zero':
+            pass0.scene_blend = 'add'
+
+        # fix the shaders
+        pass0.shaders.clear()
+        if len(pass0.texture_units) == 4: # D,E,N,S
+            del pass0.texture_units[2:] # we only require D,E
+        elif len(pass0.texture_units) == 3: # D,N,S
+            del pass0.texture_units[1:] # we only require D
+        self.add_shaders_ambient(pass0, mname)
 
         # turn ON ambient
         pass0.ambient = ogre_parse.basemodel.Color(vals=[1, 1, 1, 1])
